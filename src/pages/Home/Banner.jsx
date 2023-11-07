5f5732f929748544f8d8b2ce7c2450f9266b5044
AdnanSakib04 <navidadnan04@gmail.com>
2023-11-07 15:31:49 +0600

    banner section done

---
+++ src/pages/Home/Banner.jsx
@@ -0,0 +1,16 @@
+
+const Banner = () => {
+    return (
+        <div className="hero min-h-[85vh]" style={{ backgroundImage: 'url(https://i.ibb.co/Y7NVJ3b/image.png)' }}>
+            <div className="hero-overlay bg-opacity-60"></div>
+            <div className="hero-content text-center text-neutral-content">
+                <div className="">
+                    <h1 className="mb-5 text-2xl lg:text-5xl font-bold "><span className="text-gray-600">❝</span>Feathering Thoughts, Soaring Creativity<span className="text-gray-600">❞</span></h1>
+                   
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,10 @@
+import Banner from "./Banner";
 import NewsLetter from "./NewsLetter";
 
 const Home = () => {
     return (
         <div>
+            <Banner></Banner>
             <NewsLetter></NewsLetter>
         </div>
     );
