9a24ef6f2bb958474b891916d2dde2a8309c1782
AdnanSakib04 <navidadnan04@gmail.com>
2023-11-08 00:46:00 +0600

    filtering by category and search field added on all blogs page

--- src/pages/AllBlogs/AllBlogs.jsx
+++ src/pages/AllBlogs/AllBlogs.jsx
@@ -2,29 +2,87 @@ import { useState } from "react";
 import { useLoaderData } from "react-router-dom";
 import BlogCard from "./BlogCard";
 
-
-
 const AllBlogs = () => {
-    const loadedBlogs = useLoaderData();
-    const [blogs, setBrands] = useState(loadedBlogs);
-    return (
-        <div className='m-20'>
-        <h1 className="mt-10 text-5xl font-bold text-center mb-14  max-w-max mx-auto p-3 rounded-lg text-[#66b2b2] ">All Blogs</h1>
-        <div className="max-w-7xl mx-auto">
-        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 p-6 md:p-0  gap-y-6 mb-20 justify-items-center'>
-          {
-            blogs.map(singleBlog => <BlogCard
-                key={singleBlog._id}
-                singleBlog={singleBlog}
-                blogs={blogs}>
-            </BlogCard>)
-          }
-        
-        </div>
-        </div>
-       
-      </div>
+  const loadedBlogs = useLoaderData();
+  const [blogs, setBlogs] = useState(loadedBlogs);
+  const [selectedCategory, setSelectedCategory] = useState("all");
+  const [search, setSearch] = useState("");
+
+  const handleCategoryChange = (e) => {
+    const category = e.target.value;
+    setSelectedCategory(category);
+
+    // filtering blogs on the selected category
+    if (category === "all") {
+      setBlogs(loadedBlogs);
+    } else {
+      const filteredBlogs = loadedBlogs.filter(blog => blog.category === category);
+      setBlogs(filteredBlogs);
+    }
+  };
+
+  const handleSearch = (e) => {
+    const searchString = e.target.value;
+    setSearch(searchString);
+
+    // filtering blogs on the search title
+    const filteredBlogs = loadedBlogs.filter(blog =>
+      blog.title.toLowerCase().includes(searchString.toLowerCase())
     );
+    setBlogs(filteredBlogs);
+  };
+
+  return (
+    <div className="m-20">
+      <h1 className="mt-10 text-5xl font-bold text-center mb-14 max-w-max mx-auto p-3 rounded-lg text-gray-600">All Blogs</h1>
+      <div className="max-w-7xl mx-auto">
+
+        {/* ---------------category dropdown start------------*/}
+        <div className="flex justify-center gap-4">
+          <h1 className="font-bold text-xl text-gray-600">Filter by Category</h1>
+          <select
+            
+            className="border border-gray-600 rounded-lg p-2"
+            value={selectedCategory}
+            onChange={handleCategoryChange}>
+
+            <option value="all">all</option>
+            <option value="food">food</option>
+            <option value="health">health</option>
+            <option value="lifestyle">lifestyle</option>
+            <option value="travel">travel</option>
+
+          </select>
+        </div>
+        {/* ---------------category dropdown end------------*/}
+
+
+        {/* ------------search input start--------------*/}
+        <div className="flex justify-center gap-2 mt-3 mb-8">
+          <label htmlFor="search" className="mr-2 font-bold text-xl text-gray-600">Search by Title</label>
+          <input
+            type="text"
+            className="border border-gray-600 rounded-lg p-2"
+            value={search}
+            onChange={handleSearch}
+            placeholder="Enter blog title"/>
+        </div>
+        {/* ------------search input start--------------*/}
+
+
+        {/*all blog cards*/}
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 p-6 md:p-0 gap-y-6 mb-20 justify-items-center">
+          {blogs.map(singleBlog => (
+            <BlogCard
+              key={singleBlog._id}
+              singleBlog={singleBlog}
+              blogs={blogs}
+            />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default AllBlogs;
--- src/pages/AllBlogs/BlogCard.jsx
+++ src/pages/AllBlogs/BlogCard.jsx
@@ -1,20 +1,20 @@
 import { Link } from "react-router-dom";
 import { BiDetail } from "react-icons/bi";
-import { BsPen } from "react-icons/bs";
+import { BsBookmarks } from "react-icons/bs";
 
 const BlogCard = ({ singleBlog }) => {
     const { photo, category, shortDescription, title, _id } = singleBlog;
     return (
-        <div className="card w-96 bg-[#b2d8d8] shadow-xl">
+        <div className="card w-96 bg-gray-400 shadow-xl">
             <figure><img className="h-[213px] w-full" src={photo} alt="" /></figure>
             <div className="card-body">
-                <h2 className="text-[22px] font-bold text-gray-950">Name: {title}</h2>
-                <h2 className="text-xl font-medium text-gray-950">Brand: {category}</h2>
-                <h2 className="text-xl font-medium text-gray-950">Type: {shortDescription}</h2>
+                <h2 className="text-[22px] font-bold text-black"> {title}</h2>
+                <h2 className="text-xl font-medium text-black">Category: {category}</h2>
+                <h2 className=" text-justify"> {shortDescription}</h2>
 
                 <div className="flex justify-evenly mt-2 ">
-                    <Link to={`/productdetails/${_id}`}><button className="btn bg-[#66b2b2] text-gray-950 font-bold rounded-lg  border-none"><BiDetail></BiDetail>Details</button></Link>
-                    <Link to={`/updateproduct/${_id}`}><button className="btn font-bold text-gray-950   bg-[#66b2b2]  rounded-lg border-none"><BsPen></BsPen>Update</button></Link>
+                    <Link to={`/blogDetails/${_id}`}><button className="btn bg-blue-300 text-black font-bold rounded-lg  border-none"><BiDetail></BiDetail>Details</button></Link>
+                    <Link to={`/wishlistBlog/${_id}`}><button className="btn font-bold text-black   bg-orange-300  rounded-lg border-none"><BsBookmarks></BsBookmarks>Wishlist</button></Link>
                 </div>
             </div>
         </div>
