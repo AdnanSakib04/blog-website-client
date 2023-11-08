e0961f14ee4afba77369de7c8157fd1a57612a90
AdnanSakib04 <navidadnan04@gmail.com>
2023-11-08 20:19:13 +0600

    comment section implemented

--- src/pages/BlogDetails/BlogDetails.jsx
+++ src/pages/BlogDetails/BlogDetails.jsx
@@ -1,8 +1,9 @@
 import { Link, useLoaderData, useParams } from "react-router-dom";
 import { BsFillPencilFill } from "react-icons/bs";
 import Swal from "sweetalert2";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../../providers/AuthProvider";
+import CommentCard from "./CommentCard";
 
 
 const BlogDetails = () => {
@@ -10,7 +11,7 @@ const BlogDetails = () => {
     //console.log(blogData);
     const { id } = useParams();
     //const idInt = parseInt(id);
-    console.log("---",id);
+    console.log("---", id);
     const blog = blogData.find(blog => blog._id === id);
     const { user } = useContext(AuthContext);
     let ownersBlog;
@@ -23,24 +24,117 @@ const BlogDetails = () => {
     }
 
 
+    // -----------------comment section starts ----------------
+
+
+
+     const [comments, setComments] = useState();
+ 
+     // fetch all the comments for all the blogs
+     useEffect(() => {
+         fetch('http://localhost:5000/comments')
+           .then((res) => res.json())
+           .then((data) => setComments(data));
+       console.log(comments);
+     } );
+
+
+    const handleAddComment = event => {
+        event.preventDefault();
+
+        const form = event.target;
+
+        const comment = form.comment.value;
+        const blogId = id;
+
+        // current user email and photoURL
+        const userEmail = user.email;
+        const userPhoto = user.photoURL;
+
+        const newComment = { comment, blogId, userEmail, userPhoto };
+
+        console.log(newComment);
+
+        fetch('http://localhost:5000/addComment', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(newComment)
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                if (data.insertedId) {
+                    Swal.fire({
+                        title: 'Success!',
+                        text: 'Comment Added Successfully',
+                        icon: 'success',
+                        confirmButtonText: 'OK'
+                    })
+                }
+            })
+    }
+    // -----------------comment section ends ----------------
+
+
+
 
     return (
-        <div className="max-w-7xl p-4 mx-auto mb-40 mt-8 text-gray-950">
-            <div className="   bg-gray-400 rounded-lg  p-3">
-                <img className="w-full lg:h-[700px]" src={blog.photo} alt="" />
-                <h2 className=" text-3xl md:text-4xl lg:text-5xl font-bold text-center mt-5 mb-5 ">{blog.title}</h2>
-                <h2 className="text-base md:text-2xl font-bold text-center mb-3">Category: {blog.category}</h2>
+        <div>
+            <div className="max-w-7xl p-4 mx-auto mb-20 mt-8 text-gray-950">
+                <div className="   bg-gray-400 rounded-lg  p-3">
+                    <img className="w-full lg:h-[700px]" src={blog.photo} alt="" />
+                    <h2 className=" text-3xl md:text-4xl lg:text-5xl font-bold text-center mt-5 mb-5 ">{blog.title}</h2>
+                    <h2 className="text-base md:text-2xl font-bold text-center mb-3">Category: {blog.category}</h2>
 
-                <p className=" text-xl text-justify mb-10 font-normal"><span className=" font-bold">Short Description:</span> {blog.shortDescription}</p>
+                    <p className=" text-xl text-justify mb-10 font-normal"><span className=" font-bold">Short Description:</span> {blog.shortDescription}</p>
 
-                <p className=" text-xl text-justify mb-10  font-normal"><span className=" font-bold">Long Description: </span>{blog.longDescription}</p>
-                {ownersBlog ?
-                    <div className="flex justify-center">
-                       <Link to={`/updateBlog/${id}`}> <button className="btn bg-green-500 border-none text-xl text-gray-950"> <BsFillPencilFill></BsFillPencilFill>Update</button></Link>
-                    </div> : <></>
-                }
+                    <p className=" text-xl text-justify mb-10  font-normal"><span className=" font-bold">Long Description: </span>{blog.longDescription}</p>
+                    {ownersBlog ?
+                        <div className="flex justify-center">
+                            <Link to={`/updateBlog/${id}`}> <button className="btn bg-green-500 border-none text-xl text-gray-950"> <BsFillPencilFill></BsFillPencilFill>Update</button></Link>
+                        </div> : <></>
+                    }
+                </div>
+            </div>
+
+
+            {/* ----------------comment section-------------------- */}
+            <form onSubmit={handleAddComment} className="md:w-1/2 mx-auto card-body  rounded-3xl border-2 border-gray-600 mb-20">
+                <h1 className="text-4xl font-bold lg:text-5xl mt-8 text-center text-gray-600">Add Comment</h1>
+                <div className="form-control">
+                    <label className="label">
+                        <span className="label-text font-medium text-[18px] text-gray-600">Comment</span>
+                    </label>
+                    <input type="text" name="comment" placeholder="comment" className="input input-bordered " required />
+                </div>
+
+
+
+
+
+                <div className="form-control mt-6">
+                    <input type="submit" className="btn bg-green-500 border-none font-bold text-xl text-white" value="Add comment" />
+                </div>
+
+
+            </form >
+
+            <div className="max-w-7xl p-4 mx-auto">
+            {comments?.map(singleComment => (
+            <CommentCard
+              key={singleComment._id}
+              singleComment={singleComment}
+              comments={comments}
+            >
+            </CommentCard>
+          ))}
+          
+
             </div>
         </div>
+
     );
 };
 
---
+++ src/pages/BlogDetails/CommentCard.jsx
@@ -0,0 +1,18 @@
+
+
+const CommentCard = ({singleComment}) => {
+    const { userPhoto, userEmail, comment  } = singleComment;
+    return (
+        <div className=" mb-2">
+            <div className="border border-gray-600 p-2 rounded-3xl">
+                <div className=" flex items-center gap-4">
+                <img src={userPhoto} alt="" className=" h-[48px] w-[48px] rounded-full"/>
+                <p className=" font-medium text-xl md:text-2xl text-gray-600">{userEmail}</p>
+                </div>
+                <p className=" ml-[64px] italic text-black md:text-xl">{comment}</p>
+            </div>
+        </div>
+    );
+};
+
+export default CommentCard;
