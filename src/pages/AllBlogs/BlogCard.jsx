192d43f6f9455576f0297078e9d16af73d870a01
AdnanSakib04 <navidadnan04@gmail.com>
2023-11-09 14:57:59 +0600

    card margin updated

--- src/pages/AllBlogs/BlogCard.jsx
+++ src/pages/AllBlogs/BlogCard.jsx
@@ -11,7 +11,7 @@ const BlogCard = ({ singleBlog }) => {
     const { photo, category, shortDescription, title, _id } = singleBlog;
     console.log(_id);
 
-    const { user} = useContext(AuthContext);
+    const { user } = useContext(AuthContext);
 
 
     const handleAddToWishlist = () => {
@@ -49,18 +49,19 @@ const BlogCard = ({ singleBlog }) => {
     }
     return (
         <div className="card w-96 bg-gray-400 shadow-xl">
-            <figure><img className="h-[213px] w-full" src={photo} alt="" /></figure>
-            <div className="card-body">
-                <h2 className="text-[22px] font-bold text-black"> {title}</h2>
-                <h2 className="text-xl font-medium text-black">Category: {category}</h2>
-                <h2 className=" text-justify"> {shortDescription}</h2>
+        <figure><img className="h-[213px] w-full" src={photo} alt="" /></figure>
+        <div className="card-body">
+            <h2 className="text-[22px] font-bold text-black"> {title}</h2>
+            <h2 className="text-xl font-medium text-black">Category: {category}</h2>
+            <h2 className=" text-justify mb-2"> {shortDescription}</h2>
 
-                <div className="flex justify-evenly mt-2 ">
-                    <Link to={`/blogDetails/${_id}`}><button className="btn bg-blue-300 text-black font-bold rounded-lg  border-none"><BiDetail></BiDetail>Details</button></Link>
-                    <button onClick={handleAddToWishlist} className="btn font-bold text-black   bg-orange-300  rounded-lg border-none"><BsBookmarks></BsBookmarks>Wishlist</button>
-                </div>
+            <div className="flex justify-evenly mt-auto">
+                <Link to={`/blogDetails/${_id}`}><button className="btn bg-blue-300 text-black font-bold rounded-lg  border-none"><BiDetail></BiDetail>Details</button></Link>
+                <button onClick={handleAddToWishlist} className="btn font-bold text-black   bg-orange-300  rounded-lg border-none"><BsBookmarks></BsBookmarks>Wishlist</button>
             </div>
         </div>
+    </div>
+      
     );
 };
 
